321a1f43f0d6c49be029a0c2a7708912499fdf11
ulfhiraa <studyiraa@gmail.com>
2024-11-23 15:30:29 +0700

    GET data todos

--- routes/todo.js
+++ routes/todo.js
@@ -1,3 +1,8 @@
 // Import modul yang diperlukan
 const express = require('express');
 const router = express.Router();
+
+// Endpoint untuk mendapatkan data todos
+router.get('/', (req, res) => {
+    res.json(todos);
+});
