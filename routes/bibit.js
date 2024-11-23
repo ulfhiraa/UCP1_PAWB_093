5e76c97dca031b0bd1429dfaad6a60ad162571d3
ulfhiraa <studyiraa@gmail.com>
2024-11-23 15:25:29 +0700

    menambahkan data bibit

--- routes/bibit.js
+++ routes/bibit.js
@@ -1,3 +1,36 @@
 import express, { Route } from "express";
 
 const router = express.Router();
+
+const pupuk = [
+    {
+        id: "1",
+        namaBibit: "Bibit Cabai",
+        jenisBibit: "Cabai Merah Keriting",
+        jumlah: "2kg"
+    },
+    {
+        id: "2",
+        namaBibit: "Bibit Tomat",
+        jenisBibit: "Tomat Ceri",
+        jumlah: "1kg"
+    },
+    {
+        id: "3",
+        namaBibit: "Bibit Padi",
+        jenisBibit: "Padi Gogo",
+        jumlah: "4kg"
+    },
+    {
+        id: "4",
+        namaBibit: "Bibit Jagung",
+        jenisBibit: " Jagung Manis",
+        jumlah: "3kg"
+    },
+    {
+        id: "5",
+        namaBibit: "Bibit Kacang Panjang",
+        jenisBibit: "Kacang Panjang Hijau",
+        jumlah: "1kg"
+    }
+]
