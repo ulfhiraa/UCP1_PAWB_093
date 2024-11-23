0202a884211caa82636a81ac6c58e91c696a60dc
ulfhiraa <studyiraa@gmail.com>
2024-11-23 15:10:54 +0700

    membuat data bibit

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ import bodyParser from "body-parser";
 const app = express();
 const PORT = 8000;
 
+app.use(bodyParser.json());
+app.listen(PORT, () => 
+    console.log(
+        `Server berjalan di port : http://localhost:${PORT}`)
+);
